e5cb0a28bd597720a80d4f1e339bbe0920af2931
emanuele-casalino <emanuele.casalino.2609@gmail.com>
2023-02-12 16:58:00 +0100

    Add files via upload

--- system/consts.php
+++ system/consts.php
@@ -22,6 +22,12 @@ class Consts {
     const SIGN_UP_ON_THE_SITE_PAGE_NAME = "sign_up_on_the_site";
     const UNSUBSCRIBE_FROM_THE_SITE_PAGE_NAME = "unsubscribe_from_the_site";
     const VIEW_USER_INFO_PAGE_NAME = "view_user_info";
+    // templates
+    const PAGE_TEMPLATE_FILENAME = self::TEMPLATES_DIR . "page.php";
+    const REDIRECT_TO_HOME_TEMPLATE_FILENAME = self::TEMPLATES_DIR . "redirect_to_home.php";
+    // views
+    const ERROR_401_PAGE_FILENAME = self::VIEWS_DIR . self::HTTP_401_PAGE_NAME . ".php";
+    const ERROR_404_PAGE_FILENAME = self::VIEWS_DIR . self::HTTP_404_PAGE_NAME . ".php";
     // languages
     const DEFAULT_LANGUAGE = "en";
     const LANGUAGES = ["it", "en"];
@@ -31,11 +37,5 @@ class Consts {
     const MAX_PASSWORD_LIFETIME_LENGTH = 30 * 24 * 60 * 60;
     // sessions
     const USER_SESSION_VARIABLE_NAME = "emanuele_auth_session_user";
-    // templates
-    const PAGE_TEMPLATE_FILENAME = self::TEMPLATES_DIR . "page.php";
-    const REDIRECT_TO_HOME_TEMPLATE_FILENAME = self::TEMPLATES_DIR . "redirect_to_home.php";
-    // views
-    const ERROR_401_PAGE_FILENAME = self::VIEWS_DIR . self::HTTP_401_PAGE_NAME . ".php";
-    const ERROR_404_PAGE_FILENAME = self::VIEWS_DIR . self::HTTP_404_PAGE_NAME . ".php";
 
 }
